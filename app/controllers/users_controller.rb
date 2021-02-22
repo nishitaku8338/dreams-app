a88c041d0f9d4eb407d6f32ab19d794ec87e4f60
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-22 19:02:33 +0900

    編集情報を更新する処理を記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,19 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)        # 更新する中身(カラム)を定義する
+      redirect_to user_path(@user)      # 更新できたらユーザー詳細ページに遷移する
+    else
+      render :edit                      # 更新されなかったら編集ページにとどまる
+    end
+  end
+
+  private  # usersコントローラーのみ以下を実行する事ができる(セキュリティの強化)
+
+  def user_params  # 更新するカラムの中身
+    params.require(:user).permit(:username, :email, :profile, :profile_image)
+  end
 end
