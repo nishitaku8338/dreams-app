dd6c3ece2ff5e4aaf76c4392a0c1a522a3957933
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-22 22:27:19 +0900

    turbolinksが読み込まれないように編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()  //JavaScriptファイルの処理を正常にさせる為
 require("@rails/activestorage").start()
 require("channels")
 
