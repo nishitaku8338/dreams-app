594e1419c808b4914f6ff763aeaf0c62f8e2729f
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-23 12:33:58 +0900

    マイグレーションファイルimageカラムの削除

--- db/migrate/20210222151506_create_dreams.rb
+++ db/migrate/20210222151506_create_dreams.rb
@@ -4,7 +4,6 @@ class CreateDreams < ActiveRecord::Migration[6.0]
       t.references :user,  foreign_key: true
       t.string :title,     null: false
       t.text :body,        null: false
-      t.string :image_id,  null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2021_02_22_151506) do
     t.bigint "user_id"
     t.string "title", null: false
     t.text "body", null: false
-    t.string "image_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_dreams_on_user_id"
