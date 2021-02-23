83f3b375231830444d91941118a40e3122ab5adf
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-24 00:38:46 +0900

    ユーザー本人のみ投稿内容の編集・削除ができる(URL直入力制限)

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,12 +1,14 @@
 class DreamsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]  # deviseのメソッド！ログインしていないユーザーは投稿一覧画面のみアクセス可。
+  before_action :authenticate_user!, except: [:index, :show]            # deviseのメソッド！未登録ユーザーは投稿一覧画面と投稿詳細画面のみアクセスできる
+  before_action :set_dream, except: [:index, :new, :create]             # 単一レコードを取得する
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]  # ユーザー本人のみ投稿内容の編集・削除ができる(URL直入力制限)
 
   def index
-    @dreams = Dream.includes(:user)
+    @dreams = Dream.includes(:user)    # userを紐付け(N+1問題解決)
   end
 
   def new
-    @dream = Dream.new  # オブジェクトを生成
+    @dream = Dream.new                 # オブジェクトを生成
   end
 
   def create
@@ -19,31 +21,37 @@ class DreamsController < ApplicationController
   end
 
   def show
-    @dream = Dream.find(params[:id])
   end
 
   def edit
-    @dream = Dream.find(params[:id])
   end
 
   def update
-    @dream = Dream.find(params[:id])
-    if @dream.update(dream_params)        # 更新する中身(カラム)を定義する
-      redirect_to dream_path(@dream)      # 更新できたら投稿詳細ページに遷移する
+    if @dream.update(dream_params)     # 更新する中身(カラム)をストロングパラメーターに引き渡す
+      redirect_to dream_path(@dream)   # 更新できたら投稿詳細ページに遷移する
     else
-      render :edit                        # 更新されなかったら投稿編集ページにとどまる
+      render :edit                     # 更新されなかったら投稿編集ページにとどまる
     end
   end
 
   def destroy
-    dream = Dream.find(params[:id])
-    dream.destroy
-    redirect_to dreams_path
+    @dream.destroy
+    redirect_to dreams_path            # 投稿一覧ページに遷移する
   end
 
   private  # dreamsコントローラーの中でしか呼び出せない(セキュリティ強化)
   
-  def dream_params  # new.html.erbのフォームから送信されたカラムの内容を受け取る
+  def dream_params  # new.html.erbのフォームから送信されたカラムの内容を受け取る(ストロングパラメーター)
     params.require(:dream).permit(:title, :body, :image).merge(user_id: current_user.id)
+  end
+
+  def set_dream  # 単一レコードを取得
+    @dream = Dream.find(params[:id])
+  end
+
+  def ensure_correct_user  # ユーザー本人のみ投稿内容の編集・削除ができる(URL直入力制限)
+    if @dream.user != current_user
+      redirect_to dreams_path, alert: '不正なアクセスです。'
+    end
   end
 end
