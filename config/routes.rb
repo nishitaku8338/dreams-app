4e37036cb41ccbbb4d372bd28fabbbc6de361123
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-24 18:36:26 +0900

    ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index" # ローカルホスト3000にアクセスしたらここにアクセス
   resources :users
-  resources :dreams
+  resources :dreams do
+    resources :likes, only: [:create, :destroy]  # いいね機能のルーティング
+  end
 end
