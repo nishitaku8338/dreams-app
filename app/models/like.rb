b65df2df8b2d1e202ab6821a3a870f506ce7b713
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-24 18:03:36 +0900

    モデルのアソシエーションを記述

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,6 @@
 class Like < ApplicationRecord
+  validates_uniqueness_of :dream_id, scope: :user_id  # 有効なアクセス権を重複して付与しない
+
+  belongs_to :user   # アソシエーション
+  belongs_to :dream  # アソシエーション
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
   has_many :dreams, dependent: :destroy  # ユーザーが削除されたらdreamsの投稿も削除する
+  has_many :likes, dependent: :destroy  # 投稿が削除された時にいいねも削除する
 end
