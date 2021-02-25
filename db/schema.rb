c61754698bc27e18fa6d35fd7b55573ca1103abb
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-25 14:09:20 +0900

    マイグレーションファイル編集

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_041753) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "tweet_id"
+    t.integer "dream_id"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
