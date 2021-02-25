44a6a7ab92ebc767b6f81b9a2f7bde95af1ac777
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-25 17:13:53 +0900

    単体テストコード実行の処理を記述

--- spec/models/dream_spec.rb
+++ spec/models/dream_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Dream, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @dream = FactoryBot.build(:dream)  # factory :dreamを参照
+  end
+
+  describe '夢語新規投稿' do
+    context '新規投稿できるとき' do
+      it 'すべての値が正しく入力されていれば登録できる' do
+        expect(@dream).to be_valid
+      end
+    end
+
+    context '夢語新規投稿ができないとき' do
+      it 'image(イメージ画像)がないと登録できない' do
+        @dream.image = nil
+        @dream.valid?
+        expect(@dream.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'titleがないと登録できない' do
+        @dream.title = ''
+        @dream.valid?
+        expect(@dream.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'titleがないと登録できない' do
+        @dream.body = ''
+        @dream.valid?
+        expect(@dream.errors.full_messages).to include("Body can't be blank")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @dream.user = nil
+        @dream.valid?
+        expect(@dream.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
+
+# テストコード実行コマンド
+# bundle exec rspec spec/models/dream_spec.rb
