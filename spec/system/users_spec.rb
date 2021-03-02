e245d8420d376bd193e72497dab4cb34caeab7c2
nishitaku8338 <nishitaku8338@gmail.com>
2021-03-02 15:50:56 +0900

    テストコードを編集(ユーザーが新規登録できる時)

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,13 +7,27 @@ RSpec.describe 'ユーザー新規登録', type: :system do
   context 'ユーザー新規登録ができるとき' do 
     it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'Username', with: @user.username
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      fill_in 'Password confirmation', with: @user.password_confirmation
       # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
       # トップページへ遷移したことを確認する
-      # カーソルを合わせるとログアウトボタンが表示されることを確認する
+      expect(current_path).to eq(root_path)
+      # トップページにログアウトボタンがあることを確認する
+      expect(page).to have_content('ログアウト')
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
   context 'ユーザー新規登録ができないとき' do
@@ -27,3 +41,6 @@ RSpec.describe 'ユーザー新規登録', type: :system do
     end
   end
 end
+
+# テストコード実行コマンド
+# bundle exec rspec spec/system/users_spec.rb
